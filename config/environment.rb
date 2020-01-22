e5252ea5fe253b0d3dfc111d07480ccc78e35420
Garrett Thompson <gtcruz.gt@gmail.com>
2020-01-22 06:03:22 +0000

    Automatically backed up by Learn

--- config/environment.rb
+++ config/environment.rb
@@ -3,9 +3,4 @@ require 'pry'
 
 require_relative "../lib/student.rb"
 
-desc 'drop into the Pry console'
-task :console => :environment do
-  Pry.start
-end
-
 DB = {:conn => SQLite3::Database.new("db/students.db")}
